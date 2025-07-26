acb10d6777f37abe880d95f1a55eef58bf97e45a
ffbrunoff1 <bruno@ntcbrasil.com.br>
2025-07-26 19:29:57 -0300

    Adicionar src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import About from './components/About';
+import Services from './components/Services';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+export default function App() {
+  return (
+    <div className="bg-brand-primary text-brand-text font-sans antialiased">
+      <Header />
+      <main>
+        <Hero />
+        <About />
+        <Services />
+        <Contact />
+      </main>
+      <Footer />
+    </div>
+  );
+}
